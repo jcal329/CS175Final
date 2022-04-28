ce38114c4eef95f095f1f50b9ea6d04e3b521117
Jonathan Lindberg <jonathan.lindberg@sjsu.edu>
2022-04-27 18:06:09 -0700

    initialized empty project

---
+++ app/src/main/java/edu/sjsu/android/finalproject1/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.sjsu.android.finalproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
